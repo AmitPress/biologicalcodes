1d1b0a2065be186e56c60c2f2f1876f12f1b5759
AmitPress <amitkrsurjo@gmail.com>
2021-04-25 22:08:50 +0600

    exe must be excluded

--- biolab.cpp
+++ biolab.cpp
@@ -4,21 +4,19 @@
 
 using namespace std;
 
+void up(int x){
+    static int val;
+    if(x == 0) return;
+
+    up(--x);
+    cout<<x;
+}
+
 int main(){
     #ifndef ONLINE_JUDGE
         freopen("inputf", "r", stdin);
         freopen("outputf", "w", stdout);
     #endif
-
-    vector<float> v1 = rb::getVec<float>();
-    vector<float> v2 = rb::getVec<float>();
-    vector<float> v3 = rb::getVec<float>();
-
-    for(auto x:v1) cout<<x<<" ";
-    cout<<endl;
-    for(auto x:v2) cout<<x<<" ";
-    cout<<endl;
-    for(auto x:v3) cout<<x<<" ";
-    cout<<endl;
+    up(9);
 
 }
