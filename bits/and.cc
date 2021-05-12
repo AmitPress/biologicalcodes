dd6138fed1799937c33a01d54df2d12b36336111
AmitPress <amitkrsurjo@gmail.com>
2021-05-12 21:21:44 +0600

    next or

---
+++ bits/and.cc
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+#define vi32 vector<int>
+#define pb push_back
+
+
+/* -------- Ultimate Template for Debuggging ---------- */
+
+#define endlr cerr<<endl
+#define endln cout<<endl
+#define DBG cerr<<"Seen"<<endl
+template<typename T> void dbg_print(T x) { cerr<<x; }
+template <class T, class V> void dbg_print(pair <T, V> p) {cerr << "{"; dbg_print(p.first); cerr << ","; dbg_print(p.second); cerr << "}";}
+template<typename T> void dbg_print(vector<T> v){ cerr<<"[ "; for(T x : v) cerr<<x<<" "; cerr<<"]";}
+template<typename T, typename S> void dbg_print(map<T, S> v){ cerr<<"[ "; for(auto x : v) dbg_print(x); cerr<<" ]"; }
+
+/* ----------- -Damit ------------*/
+#ifdef amit
+#define dbg(x) cerr<<#x<<" => ", dbg_print(x);
+#else
+#define dbg(x)
+#endif
+
+/* ---------    DEBUG END      ------------  */
+
+
+
+// bitwise operation (AND)
+
+// And is used for knowing a value is whether odd or even
+
+    //      1 1 0 = 6
+    //    & 0 0 1 = 1
+    //    ------------
+    //      0 0 0 = even
+
+    // if the result is 0 then the number is even 
+    // if the result is 1 then the number is odd
+//
+
+int32_t main(){
+    #ifdef amit
+        freopen("inputf", "r", stdin);
+        freopen("outputf", "w", stdout);
+    #endif
+    int x;
+    cin>>x; // get a number to test
+    cout<<(x&1);
+    return 0;
+}
